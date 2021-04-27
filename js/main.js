80a7b55d640a53f70f51882fd885c1164a8347b0
Tincho228 <martin_quintero521@hotmail.com>
2021-04-27 15:46:49 -0300

    Style portfolio

--- js/main.js
+++ js/main.js
@@ -8,21 +8,23 @@ function change(){
 
 const links = [
     {
-      label: "Week1 notes",
+      label: "Week1 Notes",
       url: "week1/index.html"
     },
     {
-      label: "Week2 notes",
+      label: "Week2 Notes",
       url: "week2/index.html"
     }
   ]
 for (var i=0; i < links.length; i++){
 // Create an li element
 var li = document.createElement("li");
+li.setAttribute("class", "list-group-item list-group-item-action");
 // Create an a element
 var a = document.createElement("a");
 a.setAttribute("href",links[i].url);
 a.textContent = links[i].label;
+a.setAttribute("class", "text-dark");
 // Inserting a element inside the li
 li.appendChild(a);
 //Select ol list
