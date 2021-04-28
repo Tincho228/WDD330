56b608f3a7811c936c78e9c485a1e6b03d8cdaa1
Tincho228 <martin_quintero521@hotmail.com>
2021-04-28 15:42:13 -0300

    Week 2

--- js/main.js
+++ js/main.js
@@ -8,18 +8,18 @@ function change(){
 
 const links = [
     {
-      label: "Week1 Notes",
+      label: "Week1",
       url: "week1/index.html"
     },
     {
-      label: "Week2 Notes",
+      label: "Week2",
       url: "week2/index.html"
     }
   ]
 for (var i=0; i < links.length; i++){
 // Create an li element
 var li = document.createElement("li");
-li.setAttribute("class", "list-group-item list-group-item-action");
+li.setAttribute("class", "list-group-item list-group-item-action list-group-item-dark");
 // Create an a element
 var a = document.createElement("a");
 a.setAttribute("href",links[i].url);
