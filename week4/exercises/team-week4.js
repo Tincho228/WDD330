c063edd194d75d5b737bd10bb5c16aed7ba5d316
Tincho228 <martin_quintero521@hotmail.com>
2021-05-13 15:11:55 -0300

    week4

---
+++ week4/exercises/team-week4.js
@@ -0,0 +1,9 @@
+/* STEP 2 */
+
+const touchend = document.getElementById('main-board');
+
+touchend.addEventListener('touchend', touchendFunction);
+function touchendFunction(){
+    
+    console.log("change");
+}
