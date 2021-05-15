c6d6a7c251869a510d6b22bdaefa1bd769cbd778
Tincho228 <martin_quintero521@hotmail.com>
2021-05-14 23:52:17 -0300

    week4

--- week4/exercises/team-week4.js
+++ week4/exercises/team-week4.js
@@ -1,6 +1,36 @@
 /* STEP 2 */
-const boxElement = $('.box');
-console.log(Array.from(boxElement));
-Array.from(boxElement).forEach(element => {
-    console.log();
+$(document).ready(function(){
+let oTurn;
+const x = "x";
+const o = "o";
+function start(){
+const boxElement = $('.box'); /*Selecting all boxes*/ 
+Array.from(boxElement).forEach(box => {
+    box.addEventListener("touchend", handle, {once:true}); /* Assign a touchend event to every box */
 });
+}
+function handle(e){
+    const cell = e.target;
+    const currentMark = oTurn ?  o : x ;
+    placemark(cell, currentMark);
+    swapTurns();
+}
+function placemark(cell, currentMark){
+    cell.innerHTML = currentMark;
+    return;
+}
+function swapTurns(){
+    oTurn = !oTurn;
+}
+/* Reset function */
+$("button").click(function(){
+    const boxElement = $('.box');
+    Array.from(boxElement).forEach(box => {
+        box.textContent = null;
+        box.removeEventListener("touchend",handle);
+    });
+    start();
+});
+start();
+});
+
