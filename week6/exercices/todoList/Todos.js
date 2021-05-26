2e022dcabcfdc852e309569880b350cd73e93cdc
Tincho228 <martin_quintero521@hotmail.com>
2021-05-26 06:51:41 -0300

    week6

---
+++ week6/exercices/todoList/Todos.js
@@ -0,0 +1,6 @@
+/*
+Add a Todos class to the Todos.js file, and make it the default export for the module
+Descripción:
+in the constructor you should set a variable with the element our todo list will be built in,
+ and the key we will use to read/write from localStorage
+*/
