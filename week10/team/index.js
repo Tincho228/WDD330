7162420d3ca3a33cafa18d25ad51819fb287d31e
Tincho228 <martin_quintero521@hotmail.com>
2021-06-25 16:26:38 -0300

    week10

---
+++ week10/team/index.js
@@ -0,0 +1,11 @@
+import QuakesController from './QuakesController.js';
+
+import { getJSON, getLocation } from './utilities.js';
+
+
+const quakescontroller = new QuakesController('#quakeList');
+quakescontroller.init();
+
+
+
+
---
+++ week10/team/utilities.js
@@ -0,0 +1,24 @@
+const getLocation = function(options) {
+    return new Promise(function(resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+};
+
+
+function getJSON(url){
+    return fetch(url)
+        .then(function(response) {
+            if (!response.ok) {
+                throw Error(response.statusText);
+            } else {
+                return response.json();
+            }
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+}
+export {
+    getLocation,
+    getJSON
+}
